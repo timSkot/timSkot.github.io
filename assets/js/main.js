a172dd99deeade5b0165e2b6f7c16fa2167b5990
timSkot <30372741+timSkot@users.noreply.github.com>
2021-02-20 18:38:08 +0500

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,20 @@ $(document).ready(function () {
         margin:0,
         nav:true,
         dots:true,
-        items: 4
+        responsive : {
+            // breakpoint from 0 up
+            0 : {
+                items: 1,
+                dots:false
+            },
+            // breakpoint from 1199 up
+            1199 : {
+                items: 4
+            }
+        }
+    });
+    $('.hamburger').click( function() {
+        $(this).toggleClass('is-active');
+        $('.header_nav_mb').fadeToggle();
     });
 });
