c2a073d3ab7fe5398781f9fdebcb216166f660c0
timSkot <30372741+timSkot@users.noreply.github.com>
2021-02-21 21:51:34 +0500

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,4 +20,31 @@ $(document).ready(function () {
         $(this).toggleClass('is-active');
         $('.header_nav_mb').fadeToggle();
     });
+    $('.steps_wr_form_btn1').click(function(e) {
+        e.preventDefault();
+        $('.steps_wr_form_item').removeClass('active');
+        $('.steps_wr_form_item3').removeClass('active');
+        $('.steps_wr_form_item4').removeClass('active');
+        $('.steps_wr_form_item2').addClass('active');
+        $('.steps_wr_nav_item2').addClass('active');
+    });
+    $('.steps_wr_form_btn2').click(function(e) {
+        e.preventDefault();
+        $('.steps_wr_form_item').removeClass('active');
+        $('.steps_wr_form_item3').addClass('active');
+        $('.steps_wr_form_item4').removeClass('active');
+        $('.steps_wr_form_item2').removeClass('active');
+        $('.steps_wr_nav_item3').addClass('active');
+    });
+    $('.steps_wr_form_btn3').click(function(e) {
+        e.preventDefault();
+        $('.steps_wr_form_item').removeClass('active');
+        $('.steps_wr_form_item3').removeClass('active');
+        $('.steps_wr_form_item4').addClass('active');
+        $('.steps_wr_form_item2').removeClass('active');
+    });
+    $("a[href='#top']").click(function() {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
 });
